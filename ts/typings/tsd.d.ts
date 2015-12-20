8eee9af8c602ded1c85b8df0f21ad7384b7cdec5
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-12-20 17:02:38 -0600

    Unreactiying everything

--- ts/typings/tsd.d.ts
+++ ts/typings/tsd.d.ts
@@ -1,8 +1,6 @@
 
 /// <reference path="cdp-diff.d.ts" />
 /// <reference path="node/node.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="react/react.d.ts" />
 /// <reference path="rx/rx-lite.d.ts" />
 /// <reference path="rx/rx.d.ts" />
 /// <reference path="es6-promise/es6-promise.d.ts" />
